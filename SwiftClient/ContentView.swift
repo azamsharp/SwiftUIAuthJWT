4781dfa8756d5a76be842d69085588fac7414bef
Mohammad Azam <azamsharp@gmail.com>
2021-04-13 10:17:15 -0500

    Initial Commit

---
+++ SwiftClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftClient
+//
+//  Created by Mohammad Azam on 4/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftClient/SwiftClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftClientApp.swift
+//  SwiftClient
+//
+//  Created by Mohammad Azam on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
